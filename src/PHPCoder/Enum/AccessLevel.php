9d0db0b7007310cacc2625a32a80a20cdc4855b8
Alexey Puchkov <alexey@oktopost.com>
2016-10-22 18:01:32 +0300

    Remove Objection from requirments

--- src/PHPCoder/Enum/AccessLevel.php
+++ src/PHPCoder/Enum/AccessLevel.php
@@ -4,9 +4,6 @@ namespace PHPCoder\Enum;
 
 class AccessLevel
 {
-	use \Objection\TEnum;
-	
-	
 	CONST PRIVATE_ACCESS	= 'private';
 	CONST PROTECTED_ACCESS	= 'protected';
 	CONST PUBLIC_ACCESS		= 'public';
